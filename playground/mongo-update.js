becc56cf6aa15c197005722f542f86daf67e7006
Meet JS <Meets@ymail.com>
2018-12-14 14:28:55 +0530

    Added Update File

---
+++ playground/mongo-update.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb'); //Same as above line (We r Destructuring)
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/M_TodoApp',(err,db)=>
+  {
+    dbCon(err,db);
+  }
+);
+
+
+function dbCon(err,db)
+{
+  if (err)
+  {
+      return console.log("Unable to Connect to MongoDB",err);
+  }
+  console.log("Connection to MongoDB Successful");
+
+  db.collection('M_todos').findOneAndUpdate({_id: new ObjectID('5c13682285817de02292d1bc')},
+  {
+    $set:{
+            completed: true
+         }
+  },
+  {
+    returnOriginal: false
+  }
+).then(
+  (res)=>
+  {
+    console.log(res);
+  }
+);
+
+  // db.close();
+}
